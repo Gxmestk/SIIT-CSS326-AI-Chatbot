f8b11481ce0ef89934cee26ce909e6b20a126fdf
Thanaphat Khemniwat <88928353+Gxmestk@users.noreply.github.com>
2023-10-30 21:55:57 +0700

    Initial Phrase

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+// Database credentials
+$host = 'localhost';
+$db   = 'chatbot_db';
+$user = 'root';
+$pass = 'root';
+
+// Create connection
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
