4346105254cacc72c93a9533a1a6bfebab2f2d84
Thanaphat Khemniwat <88928353+Gxmestk@users.noreply.github.com>
2023-11-02 20:38:32 +0700

    refactor index.php

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Get Started</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="user_setting.css" rel="stylesheet">
-
 </head>
 
 <body class="d-flex justify-content-center align-items-center vh-100">
@@ -21,7 +17,6 @@
             <label class="form-check-label" for="darkModeToggle">Dark Mode</label>
         </div>
     </div>
-
 
     <div class="row">
         <div class="col-12 text-center" style="width: 500px;">
@@ -34,42 +29,38 @@
             <a href="sign_up.php" class="btn btn-outline-primary btn-lg w-50">Sign up</a>
         </div>
     </div>
-</body>
 
-</html>
+    <script>
+        // Array of texts to animate
+        const textArray = [
+            "drink a lot of coffee",
+            "often get asked to fix printers",
+            "listen to music while they are coding",
+            "prefer dark mode over light mode",
+            "hate interruptions during coding",
+            "love optimizing and refactoring code"
+        ];
 
+        let textIndex = 0;
+        const textElement = document.getElementById("animatedText");
+        const textWrapper = document.getElementById("animatedTextWrapper");
 
-<script>
-    // Array of texts to animate
-    const textArray = [
-        "drink a lot of coffee",
-        "often get asked to fix printers",
-        "listen to music while they are coding",
-        "prefer dark mode over light mode",
-        "hate interruptions during coding",
-        "love optimizing and refactoring code"
-    ];
+        function changeText() {
+            textWrapper.classList.remove('show'); // Remove the show class to hide
+            setTimeout(() => {
+                textElement.textContent = textArray[textIndex];
+                textWrapper.classList.add('show'); // Add the show class to display with fade effect
+                textIndex = (textIndex + 1) % textArray.length; // Cycle through the array
+            }, 300);
 
-    let textIndex = 0;
-    const textElement = document.getElementById("animatedText");
-    const textWrapper = document.getElementById("animatedTextWrapper");
+            setTimeout(changeText, 2300); // Adjust timing for fade effect and text change
+        }
 
-    function changeText() {
-        textWrapper.classList.remove('show'); // Remove the show class to hide
-        setTimeout(() => {
-            textElement.textContent = textArray[textIndex];
-            textWrapper.classList.add('show'); // Add the show class to display with fade effect
-            textIndex = (textIndex + 1) % textArray.length; // Cycle through the array
-        }, 300);
+        changeText(); // Initial call to start the animation
+    </script>
 
-        setTimeout(changeText, 2300); // Adjust timing for fade effect and text change
-    }
-
-    changeText(); // Initial call to start the animation
-
-</script>
-<script src="themeHandler.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="themeHandler.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
