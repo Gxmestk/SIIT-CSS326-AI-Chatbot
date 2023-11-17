3b5c6353cf8eb17759bb577897eda155c5c7b9ab
jjunenicha <117495161+jjunenicha@users.noreply.github.com>
2023-11-17 10:34:39 +0700

    Add files via upload

--- setting.php
+++ setting.php
@@ -1,51 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="chat_style.css" rel="stylesheet"> <!-- External CSS file for chat page styles -->
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 20px;
-        }
-
-        #profile-container {
-            max-width: 600px;
-            margin: auto;
-        }
-
-        h2 {
-            color: #333;
-        }
-
-        p {
-            margin-bottom: 20px;
-        }
-
-        #button_edit,
-        #button_delete {
-            width: 100%; /* Make buttons take up the full width */
-        }
-    </style>
+    <title id="title_page">Profile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="user_setting.css" rel="stylesheet">
 </head>
+
 <body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-10 col-md-8 col-lg-6">
+                <div class="card mx-auto my-5">
+                    <div class="card-body">
+                        <h3 class="text-center mb-4" id="header_new_page">Profile</h3>
 
-<div id="profile-container" class="mx-auto text-center">
-    <h2>User Profile</h2>
-    <br>
-    <p><strong>FirstName:</strong> John <strong>LastName:</strong> D</p>
-    <p><strong>Email:</strong> john.doe@example.com</p>
-    <p><strong>Phone Number:</strong> 0890000000</p>
-    <p><strong>Birthdate:</strong> 01/01/2000</p>
-    
-    <a href="edit.php" class="btn btn-outline-primary btn-lg mb-2 w-50 " id="button_edit">Edit Profile</a>
-    <br>
-    <a href="delete.php" class="btn btn-outline-primary btn-lg mb-2 w-50" id="button_delete">Delete Account</a>
-    
-</div>
+                        <!-- Personal Information Section -->
+                        <div class="mb-3">
+                            <div class="row g-2 d-flex justify-content-center align-items-center">
+                                <div class="col border">
+                                    <p><strong>FirstName:</strong> </p>
+                                </div>
+                                <div class="col border">
+                                    <p><strong>LastName:</strong> </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-3 border p-1">
+                            <p><strong>Email:</strong> </p>
+                        </div>
+                        <div class="mb-3 border p-1">
+                            <p><strong>Phone Number:</strong> </p>
+                        </div>
+                        <div class="mb-3 border p-1">
+                            <p><strong>Birthdate:</strong> .</p>
+                        </div>
+                        <!-- End of Personal Information Section -->
 
+                        <!-- Edit and Delete buttons -->
+                        <div class="d-flex justify-content-center align-items-center">
+                            <a href="edit.php" class="btn btn-primary flex-grow-1" id="button_edit">Edit Profile</a>
+                            <a href="delete.php" class="btn btn-primary flex-grow-1" id="button_delete">Delete Account</a>
+                        </div>
+                        <!-- End of Edit and Delete buttons -->
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
