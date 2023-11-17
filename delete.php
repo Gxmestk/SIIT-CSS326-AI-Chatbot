3da8eba782fa6b331e0708ab8014f534999238ea
jjunenicha <117495161+jjunenicha@users.noreply.github.com>
2023-11-17 13:42:19 +0700

    Add files via upload

--- delete.php
+++ delete.php
@@ -25,9 +25,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <body class="d-flex justify-content-center align-items-center vh-100">
 
+    <!-- Top-right corner language switch and dark mode toggle -->
+    <div class="position-absolute top-0 end-0 p-3">
+        <div class="d-flex align-items-center"> <!-- Flex container for inline alignment of elements -->
+            <!-- Language Switch Buttons -->
+            <div class="btn-group" role="group" aria-label="Language switch">
+                <button type="button" class="btn btn-outline-secondary btn-sm btn-language" id="btn-language-th">TH</button> <!-- Button for Thai language -->
+                <button type="button" class="btn btn-outline-secondary btn-sm btn-language" id="btn-language-en">EN</button> <!-- Button for English language -->
+            </div>
+
+            <!-- Dark Mode Toggle Switch -->
+            <div class="form-check form-switch ms-3">
+                <input class="form-check-input" type="checkbox" id="darkModeToggle"> <!-- Checkbox input for dark mode toggle -->
+                <label class="form-check-label" for="darkModeToggle" id="label_dark_mode">Dark Mode</label> <!-- Label for the dark mode toggle -->
+            </div>
+        </div>
+    </div>
+
  <div class="row"> <!-- Bootstrap row for layout -->
         <div class="col-12 text-center" style="width: 500px;"> <!-- Column with centered text and fixed width -->
-            <h1 class="mb-2"> Are you sure you want to delete your account?</h1> <!-- Main heading -->
+            <h1 class="mb-2" id="delete_title"> Are you sure you want to delete your account?</h1> <!-- Main heading -->
             <br>    
             <a href=" " class="btn btn-outline-primary btn-lg mb-2 w-50" id="button_yes">YES</a> <!-- Link styled as button for logging in -->
             <br> <!-- Line break -->
@@ -35,6 +52,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
     </div>
 
+    <script src="themeHandler.js"></script> <!-- A script link to a JavaScript file for theme handling, likely related to dark/light mode switching -->
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
